169b9ed2c6c95621c97b8a709a131bf8778bc0a1
TomFong <tom.fonglamhang@gmail.com>
2021-05-05 18:07:31 +0800

    add new service

---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(
+            CustomerLookupModel request, 
+            ServerCallContext context
+        )
+        {
+            CustomerModel output = new CustomerModel();
+
+            output.FirstName = "Tom";
+            output.LastName = "Fong";
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(
+            NewCustomerRequest request,
+            IServerStreamWriter<CustomerModel> responseStream,
+            ServerCallContext context
+        )
+        {
+            List<CustomerModel> customers = new List<CustomerModel>();
+
+            
+
+            foreach (var customer in customers)
+            {
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
--- GrpcServer/Startup.cs
+++ GrpcServer/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using GrpcServer.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +33,7 @@ namespace GrpcServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<CustomerService>();
 
                 endpoints.MapGet("/", async context =>
                 {
